c8a34ee30b86a3a1ec78e30f3c1216c3e26d5ede
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-04-20 20:10:36 +0200

    init

--- src/main/java/pl/jwrabel/trainings/algorithms/sorting/SelectionSort.java
+++ src/main/java/pl/jwrabel/trainings/algorithms/sorting/SelectionSort.java
@@ -5,7 +5,39 @@ package pl.jwrabel.trainings.algorithms.sorting;
  */
 public class SelectionSort implements SortingAlgorithm {
     public int[] sort(int[] data) {
-        return new int[]{1, 2, 3};
+        for (int i = 0; i < data.length - 1; i++) {
+            int indexOfMin = findIndexOfMin(data, i);
+
+            int tmp = data[i];
+            data[i] = data[indexOfMin];
+            data[indexOfMin] = tmp;
+        }
+        
+        return data;
+    }
+
+    int findIndexOfMin(int[] data, int fromIndex) {
+        int minIndex = fromIndex;
+
+        for (int i = fromIndex + 1; i < data.length; i++) {
+            if (data[i] < data[minIndex]) {
+                minIndex = i;
+            }
+        }
+
+        return minIndex;
+    }
+
+    int findMin(int[] data) {
+        int min = data[0];
+
+        for (int i = 1; i < data.length; i++) {
+            if (data[i] < min) {
+                min = data[i];
+            }
+        }
+
+        return min;
     }
 
     @Override
