3e227f2f494909d1db2810037acee01f7c02260d
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-04-20 20:41:35 +0200

    init

--- src/main/java/pl/jwrabel/trainings/algorithms/sorting/InsertionSort.java
+++ src/main/java/pl/jwrabel/trainings/algorithms/sorting/InsertionSort.java
@@ -10,19 +10,18 @@ public class InsertionSort implements SortingAlgorithm {
     public int[] sort(int[] data) {
         List<Integer> list = new ArrayList<>();
 
+//        PĘTLA FOR-EACH
 //        for (int elem : data) {
 //            // KOD
 //        }
 //        // TO TO SAMO CO
 //        for (int i = 0; i < data.length; i++) {
 //            int elem = data[i];
-//
 //            // KOD
-//
 //        }
 
-        // wstawienie wartości 10 do listy pod indeksem 3
-//        list.add(3, 10);
+//      wstawienie wartości 10 do listy pod indeksem 3
+//      list.add(3, 10);
 
         for (int elem : data) {
 
