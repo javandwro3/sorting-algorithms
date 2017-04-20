05342917a128c82446f7cb6af5540d584fce94ce
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-04-20 20:38:18 +0200

    init

--- src/main/java/pl/jwrabel/trainings/algorithms/sorting/InsertionSort.java
+++ src/main/java/pl/jwrabel/trainings/algorithms/sorting/InsertionSort.java
@@ -1,11 +1,37 @@
 package pl.jwrabel.trainings.algorithms.sorting;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by jakubwrabel on 16.12.2016.
  */
 public class InsertionSort implements SortingAlgorithm {
     public int[] sort(int[] data) {
-        return new int[]{1, 2, 3};
+        List<Integer> list = new ArrayList<>();
+
+//        for (int elem : data) {
+//            // KOD
+//        }
+//        // TO TO SAMO CO
+//        for (int i = 0; i < data.length; i++) {
+//            int elem = data[i];
+//
+//            // KOD
+//
+//        }
+
+        for (int elem : data) {
+
+            // wstawianie do listy
+            // w poprawnej kolejności
+
+        }
+
+        for (int i = 0; i < data.length; i++) {
+            data[i] = list.get(i);
+        }
+        return data;
     }
 
     @Override
